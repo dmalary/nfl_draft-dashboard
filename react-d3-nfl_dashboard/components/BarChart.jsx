7667129310abcbb7072625ae9c758567ab657766
madhat5 <davidmalary@gmail.com>
2024-03-27 15:11:52 -0400

    feat: add working data filter, add basic bar + axes

--- react-d3-nfl_dashboard/components/BarChart.jsx
+++ react-d3-nfl_dashboard/components/BarChart.jsx
@@ -1,24 +1,68 @@
-const barWidth = 60;
+import { useEffect } from "react";
+import * as d3 from 'd3';
 
-const barChart = ({ width, height, data }) => {
+const margin = {
+  top: 30,
+  bottom: 70,
+  left: 50,
+  right: 25,
+}
+
+const BarChart = ({ width, height, data }) => {
+
+  const bottom = height - margin.bottom;
+
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.round)) // set with dropdown (years, round)
+    .range([margin.left, width - margin.right])
+    .padding(.1)
+
+  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.yearsPlayed)]) // set with dropdown (position, all)
+    .nice()
+    .range([bottom, margin.top])
+
+  const yAxis = d3.axisLeft(yScale);
+
+  useEffect(() => {
+    d3.select(".x-axis")
+      .call(xAxis)
+      .selectAll("text")
+      .attr("font-size", "14px")
+      // Rotate the labels to make them easier to read.
+      .attr("transform", "rotate(-45)")
+      .attr("text-anchor", "end");
+    
+    d3.select(".y-axis")
+      .call(yAxis)
+      .selectAll("text")
+      .attr("font-size", "14px");
+  }, [xAxis, yAxis])
+
   return (
     <div className="container">
       <svg className="viz" width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
         <g className="bars">
           {data.map((d, i) => (
             <rect
-              key={i}
-              width={barWidth}
-              height={d}
-              x={i * (barWidth + 5)}
-              y={height - d}
+              key={d.key && d.key}
+              width={xScale.bandwidth()}
+              height={bottom - yScale(d.yearsPlayed)}
+              x={xScale(d.round)}
+              y={yScale(d.yearsPlayed)}
               fill="#6baed6"
             />
           ))}
         </g>
+        <g className="x-axis" transform={`translate(0,${bottom})`}></g>
+        <g className="y-axis" transform={`translate(${margin.left},0)`}></g>
       </svg>
     </div>
   )
 };
 
-export default barChart;
+export default BarChart;
--- react-d3-nfl_dashboard/src/App.jsx
+++ react-d3-nfl_dashboard/src/App.jsx
@@ -5,13 +5,17 @@ import BarChart from '../components/BarChart';
 
 import data from '../data/data.json'
 
-const testData = [130, 200, 170, 140, 130, 250, 160];
+// const testData = [130, 200, 170, 140, 130, 250, 160];
 
 function App() {
-  console.log('data', data);
-
+  // console.log('data', data);
+  
+  const workingData = data.filter(d => d.year === 2001 && d.position === "LB" && d.position) // update with dropdown
+  console.log('workingData', workingData);
+  
   return (
-    <BarChart width={450} height={300} data={testData}/>
+    // add navbar that filters & returns data from dropdown with hooks 
+    <BarChart width={800} height={600} data={workingData}/>
   )
 }
 

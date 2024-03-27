eb17a74312e2579659f0e27de808638abb1e15e1
madhat5 <davidmalary@gmail.com>
2024-03-27 19:02:40 -0400

    fix: hide imports for errors

--- react-d3-nfl_dashboard/components/NavBar.jsx
+++ react-d3-nfl_dashboard/components/NavBar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import DropdownFilter from '../components/DropdownFilter';
 
--- react-d3-nfl_dashboard/src/App.jsx
+++ react-d3-nfl_dashboard/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 import BarChart from '../components/BarChart';

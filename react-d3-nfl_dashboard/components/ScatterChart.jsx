267f68b7464f82b9aa9eb6111b28f88bc5fdf015
madhat5 <davidmalary@gmail.com>
2024-04-08 10:34:05 -0400

    fix: tooltip component update

--- react-d3-nfl_dashboard/components/ScatterChart.jsx
+++ react-d3-nfl_dashboard/components/ScatterChart.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useMemo } from "react";
 import * as d3 from 'd3';
 import { Delaunay } from "d3";
 
-// import Tooltip from "../components/Tooltip";
+import Tooltip from "../components/Tooltip";
 // import ChartLegend from '../components/ChartLegend';
 
 const margin = {
@@ -167,19 +167,7 @@ const ScatterChart = ({ width, height, data }) => {
       </svg>
       {/* TOOLTIP */}
       { interactData && 
-        <div
-          className="tooltip"
-          style={{
-            left: interactData.xPos + 350, // Adjust as needed
-            top: interactData.yPos + 80, // Adjust as needed
-            width: 220, // Set the width
-            height: 75, // Set the height
-            pointerEvents: "none", // To prevent the tooltip from blocking mouse events
-          }}
-        >
-          <p>{interactData.position} <b>{interactData.name}</b> ({interactData.college})</p>
-          <p><i>{interactData.year} pick: {interactData.pick} (round {interactData.round})</i></p>
-        </div>
+        <Tooltip interactData={interactData}/>
       }
     </div>
   )
--- react-d3-nfl_dashboard/components/Tooltip.jsx
+++ react-d3-nfl_dashboard/components/Tooltip.jsx
@@ -1,23 +1,25 @@
 /* eslint-disable react/prop-types */
 
-import styles from "../styles/tooltip.module.css";
-
 const Tooltip = ({ interactData }) => {
   if (!interactData) {
     return null;
   }
 
-  console.log('interactData', interactData)
+  // console.log('interactData', interactData)
 
   return (
     <div
-      className={styles.tooltip}
+      className="tooltip"
       style={{
-        left: interactData.xPos,
-        top: interactData.yPos,
+        left: interactData.xPos + 280, // Adjust as needed
+        top: interactData.yPos + 80, // Adjust as needed
+        width: 220, // Set the width
+        height: 75, // Set the height
+        pointerEvents: "none", // To prevent the tooltip from blocking mouse events
       }}
     >
-      {interactData.name}
+      <p>{interactData.position} <b>{interactData.name}</b> ({interactData.college})</p>
+      <p><i>{interactData.year} pick: {interactData.pick} (round {interactData.round})</i></p>
     </div>
   );
 };
